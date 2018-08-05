ea7fa854262b0a37575ad44e12d89beda2ac986d
Allen Byerly <allen@allenbyerly.com>
2018-08-05 16:55:07 -0700

    added capability for indeed job search based on trinet and initial csv output.... note this is a very rough script but it works

--- leadleech.py
+++ leadleech.py
@@ -14,33 +14,41 @@ with open("peo_leads.csv", "w") as f:
     f.write("union_type,union_name,union_location,union_members,union_detail_href\n")
     
     #generate sources
-    for x in range(1,46):
+    for x in range(1,50):
         print x
-        urls.append("http://www.unions.org/union_search.php?&abbr=&abbr_key=&bizcat=0&zip=95230&local=&zip_key=95230&pagenum=" + repr(x))
+        urls.append("https://www.indeed.com/jobs?q=trinet&start=" + repr(x))
 
     #process sources
     for url in urls:
         html = requests.get(url).content
         soup = BeautifulSoup(html)
+      	
+      	#print soup
+
+        with open("peo_leads.html", "w") as h:
 
         #EXTRACT results from the the source
-        results = soup.findAll(attrs = {"class" : "searchResult"  })
-
+        	results = soup.findAll('a', attrs={'class' : 'jobtitle turnstileLink'})
+        	#h.write(html)
+        	h.write(html)
+        #print results
        	#TRANSFORM results
         for result in results:
-            union_type = result.a.text.split(" - ")[0]
-            union_name = result.a.text.split(" - ")[1]
-            union_location = result.p.text.split("Map", 1)[0]
-            union_members = result.p.text.split("Members: ", 1)[1]
-            union_detail_href = "http://www.unions.org" + result.a.get('href')
+          #  print result
+            #union_type = result.a.text.split(" - ")[0]
+            #union_name = result.a.text.split(" - ")[1]
+            #union_location = result.p.text.split("Map", 1)[0]
+            #union_members = result.p.text.split("Members: ", 1)[1]
+            #union_detail_href = "http://www.unions.org" + result.a.get('href')
             
             #LOAD results into the data store
-            union_record = union_type + "," + union_name + "," + union_location + "," + union_members + "," + union_detail_href
-            print union_record
-            f.write(union_record + "\n")
+            lead_link = "https://www.indeed.com" + result['href']
+            print lead_link
+            lead_record = lead_link#union_type + "," + union_name + "," + union_location + "," + union_members + "," + leadlink
+           # print union_record
             
-
-
+            f.write(lead_record + "\n")
+        print "DONE"
 #<h3><a href="/unions/american-federation-of-government-employees/local-1208/18740" title="AFGE - GOVERNMENT EMPLOYEES Local 1208">AFGE - GOVERNMENT EMPLOYEES Local 1208</a></h3>
 #<p> <br />SANGER, CA 936579737 <a target="_blank" href="http://maps.google.com/maps?q=+SANGER%2C+CA+93657" class="xSmall blue" title="View map">Map</a>
 #<br />ELODIA CASTRO                            <br />Union Members: 49                                        </p>
