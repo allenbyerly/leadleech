52935c0bd2eeafb2b3a231cde7c7cbd7438847c6
Allen Byerly <allen@allenbyerly.com>
2018-08-08 13:35:52 -0700

    first version complete... use leadleech2.py

--- leadleech2.py
+++ leadleech2.py
@@ -7,7 +7,7 @@ from BeautifulSoup import BeautifulSoup
 import requests
 import re
 
-urls = []
+
 
 f = open('leadlist.txt', "r")
 
@@ -26,34 +26,37 @@ with open("peo_leads.csv", "w") as f:
     f.write("company,link,keyword\n")
     
     for line in lines:
+        print line
         #generate sources
-        for x in range(0,1):
-            
+        urls = []
+
+        for x in range(0,10):
+            print x
             x=x*10
             #print x
             urls.append("https://www.indeed.com/jobs?q=" + line.strip() + "&start=" + repr(x))
 
             #process sources
-            for url in urls:
-             #   print url
-                html = requests.get(url).content
-                soup = BeautifulSoup(html)
-                
-                #EXTRACT results from the the source
-                results = soup.findAll('div', {'class' : re.compile('.*row.*result.*')})
+        for url in urls:
+         #   print url
+            html = requests.get(url).content
+            soup = BeautifulSoup(html)
+            
+            #EXTRACT results from the the source
+            results = soup.findAll('div', {'class' : re.compile('.*row.*result.*')})
 
-                for result in results: 
-                    print result
-                    lead_keyword = line.strip()
-                    lead_link = "https://www.indeed.com" + result.a['href']
-                    #print lead_link
-                    lead_company = result.span.text
-                   # print lead_company
+            for result in results: 
+                #print result
+                lead_keyword = line.strip()
+                lead_link = "https://www.indeed.com" + result.a['href']
+                #print lead_link
+                lead_company = result.span.text
+               # print lead_company
 
-                    lead_record = lead_company + "," + lead_link + "," + lead_keyword
-                    # print union_record
-                    print lead_record
-                    f.write(lead_record + "\n")
+                lead_record = lead_company + "," + lead_link + "," + lead_keyword
+                # print union_record
+                #print lead_keyword
+                f.write(lead_record + "\n")
                 #for result in results: 
                 #print lead_link
                # lead_company = soup.div.span
